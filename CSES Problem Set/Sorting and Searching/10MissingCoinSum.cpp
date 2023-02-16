b2e690650111f2ca5bf186a98f699f39e4a5b590
Piyush Kumar <83285872+piyushkumarg@users.noreply.github.com>
2023-02-16 23:06:19 +0530

    Create 10MissingCoinSum.cpp

---
+++ CSES Problem Set/Sorting and Searching/10MissingCoinSum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ipair pair<int, int>
+#define llpair pair<ll, ll>
+#define mp make_pair
+#define f first
+#define s second
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+#define M 1000000007
+const int N = 1e6 + 10;
+
+ll solve(vector<ll> &arr, int &n)
+{
+    sort(arr.begin(), arr.end());
+    ll res = 1;
+    for (auto &x : arr)
+    {
+        if (x <= res)
+            res += x;
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (auto &x : arr)
+    {
+        cin >> x;
+    }
+    cout << solve(arr, n);
+    return 0;
+}
